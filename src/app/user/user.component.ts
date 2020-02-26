c144775f732916c58cdaea5ce15e4db62514b5dd
Kostyanp95 <kostyanp95@yandex.ru>
2020-02-26 16:55:59 +0300

    Создал компоненту юзера

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,36 @@
+import { User } from '../app.component';
+import { 
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class UserComponent implements OnInit {
+  
+  @Input() user: User
+  @Output() onRemove: EventEmitter<number> = new EventEmitter<number>()
+  @Output() onEdit: EventEmitter<number> = new EventEmitter<number>()
+
+  ngOnInit(): void { }
+
+  removeUser() {
+    this.onRemove.emit(this.user.id)
+    console.log('Remove user ', this.user.id)
+  }
+
+  changeActiveUser() {
+    this.onEdit.emit(this.user.id)
+    console.log('Edited user ', this.user.id)
+  }
+
+}
