ba6085d585d89272eb8306ddaff315053bd95043
Kostyanp95 <kostyanp95@yandex.ru>
2020-02-26 16:54:54 +0300

    Подключил необходимые модули

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from 'src/app/app.component';
+import { CreatingUserComponent } from './creating-user/creating-user.component';
+import { UserComponent } from './user/user.component';
+import { EditingUserComponent } from './editing-user/editing-user.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreatingUserComponent,
+    UserComponent,
+    EditingUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule, 
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
